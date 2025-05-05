c2d8aacf1114abc499f65d25740a58dcaf5298d8
sgonzalez <sgonzalez@106-03.info2>
2025-05-05 12:41:08 +0200

    1

--- Tema7/src/ejercicio1/Ejercicio1.java
+++ Tema7/src/ejercicio1/Ejercicio1.java
@@ -1,5 +1,38 @@
 package ejercicio1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Ejercicio1 {
+	public static void main(String[] args) {
+
+		String url = "jdbc:mysql://localhost/institutodb";
+		String usuario = "root";
+		String contraseña = "Nuevacuenta13*";
+
+		try (Connection con = DriverManager.getConnection(url, usuario, contraseña)) {
+			
+			Statement st=con.createStatement();
+		
+			//Query solo hace selects
+			ResultSet res=st.executeQuery("select * from estudiantes");
+			
+			//update hace insert, update y delete
+			//st.executeUpdate("insert....");
+
+			
+			while(!res.next()) {
+				System.out.println(res.getS);
+			}
+			
+			
+		} catch (SQLException e) {
+
+			System.out.println("Error con la base de datos");
+		}
+	}
 
 }
