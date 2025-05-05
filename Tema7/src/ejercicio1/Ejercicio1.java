95993819ba606231f0c2fcd13a11d4d2ef986c97
Sergio <Sergio@192.168.1.134>
2025-05-05 20:34:35 +0200

    1

--- Tema7/src/ejercicio1/Ejercicio1.java
+++ Tema7/src/ejercicio1/Ejercicio1.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
+import com.mysql.cj.xdevapi.Statement;
 
 public class Ejercicio1 {
 	public static void main(String[] args) {
