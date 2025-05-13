4e737cd5fcf27c212bb975dd0a2b2d94859e0752
sgonzalez <sgonzalez@106-03.info2>
2025-05-13 14:28:55 +0200

    1 terminado

--- Tema7/src/ejercicio1/Ejercicio1.java
+++ Tema7/src/ejercicio1/Ejercicio1.java
@@ -2,36 +2,91 @@ package ejercicio1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
+import utiles.Constantes;
 
 public class Ejercicio1 {
 	public static void main(String[] args) {
 
-		String url = "jdbc:mysql://localhost/institutodb";
-		String usuario = "root";
-		String contraseña = "Nuevacuenta13*";
+		// conexión con la base de datos dentro de un try catch
+		try (Connection con = DriverManager.getConnection(Constantes.URL, Constantes.USUARIO, Constantes.CONTRASEÑA)) {
+			// consulta de media de nota donde tenemos en cuenta el id del curso y la nota
+			String mediaNotas = "select * from calificaciones where id_curso=? and nota>?";
 
-		try (Connection con = DriverManager.getConnection(url, usuario, contraseña)) {
-			
-			Statement st=con.createStatement();
-		
-			//Query solo hace selects
-			ResultSet res=st.executeQuery("select * from estudiantes");
-			
-			//update hace insert, update y delete
-			//st.executeUpdate("insert....");
+			// guardamos la sentencia insert por agilizar con los datos que vamos a insertar
+			String insert = "insert into estudiantes(nombre, apellido, fecha_nacimiento, email, telefono)"
+					+ "values(?,?,?,?,?)";
 
-			
-			while(!res.next()) {
-				System.out.println();
+			// sentencia delete
+			String delete = "delete from estudiantes where nombre='Paco'";
+
+			// sentencia paremetrizada creando objeto de tipo PreparedStantement
+			PreparedStatement pst = con.prepareStatement(mediaNotas);
+			// le indicamos que le primer interrogante que asignamos(el 1) y que el valor
+			// que le corresponde es 2
+			pst.setInt(1, 2);
+			// le indicamos que le primer interrogante que asignamos(el 2) y que el valor
+			// que le corresponde es 8
+			pst.setDouble(2, 8);
+
+			// guardamos el resultado de la consulta
+			ResultSet rs = pst.executeQuery();
+
+			// recorremos el resultado
+			while (rs.next()) {
+				// imprimimos el getInt(el tipo) de la columna 2 que equivale a la id de
+				// estudiante
+				System.out.print("ID estudiante: " + rs.getInt(2) + " ");
+				// imprimimos el getInt (el tipo) de la columna 3 que equivale a la ide de curso
+				System.out.print("ID curso: " + rs.getInt(3) + " ");
+				// imprimimos el getDouble (el tipo) de la columna 6 la nota media
+				System.out.println("Nota media: " + rs.getDouble(6));
 			}
-			
-			
-		} catch (SQLException e) {
 
-			System.out.println("Error con la base de datos");
+			// objeto PreparedStatment y le pasamos el primer insert
+			pst = con.prepareStatement(insert);
+			//valores de la inserción
+			pst.setString(1, "Elva");
+			pst.setString(2, "Ginón");
+			pst.setString(3, "2005-04-18");
+			pst.setString(4, "muchamarcha@gmail.com");
+			pst.setString(5, "6547483823");
+			//guardamos cuantas filas se han modificado
+			int res = pst.executeUpdate();
+
+			// objeto PreparedStatment y le pasamos el primer insert
+			pst = con.prepareStatement(insert);
+			//valores de la inserción
+			pst.setString(1, "Manolo");
+			pst.setString(2, "El del Bombo");
+			pst.setString(3, "1095-03-10");
+			pst.setString(4, "bombo@gmail.com");
+			pst.setString(5, "652782663");
+			
+			//sumamos las filas modificadas
+			res += pst.executeUpdate();
+
+			// objeto PreparedStatment y le pasamos el primer insert
+			pst = con.prepareStatement(insert);
+			pst.setString(1, "María");
+			pst.setString(2, "Unpajote");
+			pst.setString(3, "2005-01-01");
+			pst.setString(4, "maritrini@gmail.com");
+			pst.setString(5, "736782663");
+			
+			//sumamos filas modificadas
+			res += pst.executeUpdate();
+
+			// imprimimos la respuesta
+			System.out.println("Fila modificada " + res);
+
+			// capturamos excepción sql
+		} catch (SQLException e) {
+			// mensajito error
+			System.out.println("Error con la base de datos " + e.getMessage());
 		}
 	}
 
---
+++ Tema7/src/utiles/Constantes.java
@@ -0,0 +1,10 @@
+package utiles;
+
+public class Constantes {
+
+	public final static String URL = "jdbc:mysql://localhost/institutodb";
+	public final static String USUARIO = "root";
+	public final static String CONTRASEÑA = "Nuevacuenta13*";
+	
+	
+}
