d026565a63ba32dbdf5b11adb8d102775f3d4106
sgonzalez <sgonzalez@106-03.info2>
2025-05-16 13:19:26 +0200

    10

--- Tema7/src/ejercicio9/Ejercicio9.java
+++ Tema7/src/ejercicio9/Ejercicio9.java
@@ -14,23 +14,22 @@ public class Ejercicio9 {
 		// conexión con la base de datos dentro de un try catch
 		try (Connection con = DriverManager.getConnection(Constantes.URL, Constantes.USUARIO, Constantes.CONTRASEÑA)) {
 			// consulta de media de nota donde tenemos en cuenta el id del curso y la nota
-			String select = "select nombre and fecha_nacimiento from estudiantes";
+			String select = "select nombre, fecha_nacimiento from estudiantes";
 			String cadena;
 
 			// sentencia paremetrizada creando objeto de tipo PreparedStantement
 			PreparedStatement pst = con.prepareStatement(select);
-		
+
 			// guardamos el resultado de la consulta
 			ResultSet rs = pst.executeQuery();
 
 			// recorremos el resultado
 			while (rs.next()) {
-			
-			
-				System.out.println(rs.getString(1));
-			}
 
-		
+				// imprimimos la un getString de la respuesta y le indicamos la tabla nombre y
+				// fecha de nacimiento
+				System.out.println(rs.getString("nombre") + " " + rs.getString("fecha_nacimiento"));
+			}
 
 			// capturamos excepción sql
 		} catch (SQLException e) {
