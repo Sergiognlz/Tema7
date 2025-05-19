12edf3d3dd126570962274cfb3c9fa99218bcbdb
sgonzalez <sgonzalez@106-03.info2>
2025-05-19 12:01:54 +0200

    +

---
+++ Tema7/src/pruebasCRUD/Alumno.java
@@ -0,0 +1,29 @@
+package pruebasCRUD;
+
+import java.util.Date;
+
+/**
+ * objeto alumno
+ */
+public class Alumno {
+/**
+ * id del alumno
+ */
+private int id;
+/**
+ * nombre alumno
+ */
+private String nombre;
+/**
+ * 
+ */
+private Date fNacimiento;
+
+private double notaMedia;
+
+private String curso;
+
+
+
+	
+}
